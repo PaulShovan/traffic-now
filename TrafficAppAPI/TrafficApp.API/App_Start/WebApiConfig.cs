badfeb06ba953085c97aa512d3a4d4dbd8f635be
gypsyShovan <gypsy.shovan@gmail.com>
2015-11-04 00:34:06 +0600

    missing file add

---
+++ TrafficAppAPI/TrafficApp.API/App_Start/WebApiConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace TrafficApp.API
+{
+    public static class WebApiConfig
+    {
+        public static HttpConfiguration Register()
+        {
+            var config = new HttpConfiguration();
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+            config.Routes.MapHttpRoute(name: "DefaultRouting",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional });
+            //config.Formatters.XmlFormatter.SupportedMediaTypes.Clear();
+            config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+            //config.Formatters.Add(new UploadMultipartMediaTypeFormatter());
+            return config;
+        }
+    }
+}

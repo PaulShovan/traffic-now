bbe982d1e48db9454eeae895dbdd05fd1d2d3963
gypsyShovan <gypsy.shovan@gmail.com>
2016-06-22 01:19:28 +0600

    update map point response data model and add time filter for last 24 hours

--- TrafficNow/TrafficNow.Service.Implementation/MapService.cs
+++ TrafficNow/TrafficNow.Service.Implementation/MapService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using TrafficNow.Core.Helpers;
 using TrafficNow.Model.Map;
 using TrafficNow.Repository.Interface.Map;
 using TrafficNow.Service.Interface;
@@ -10,16 +11,20 @@ namespace TrafficNow.Service.Implementation
     public class MapService : IMapService
     {
         private IMapRepository _mapRepository;
+        private Utility _utility;
         public MapService(IMapRepository mapRepository)
         {
             _mapRepository = mapRepository;
+            _utility = new Utility();
         }
 
         public async Task<List<LocationViewModel>> GetMapPoints(double lat, double lon, double rad)
         {
             try
             {
-                return await _mapRepository.GetMapPoints(lat, lon, rad);
+                var timeTo = _utility.GetTimeInMilliseconds();
+                var timeFrom = timeTo - 86400000;
+                return await _mapRepository.GetMapPoints(lat, lon, rad, timeFrom, timeTo);
             }
             catch (Exception e)
             {
